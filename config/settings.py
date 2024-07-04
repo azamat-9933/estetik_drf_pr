0b5cc72d8bb1d78820bfdc361d4aa8be6d795f45
azamat-9933 <azamat.yakhyayeff@gmail.com>
2024-07-04 17:31:19 +0500

    'thc'

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': config["DB_ENGINE"],
-        'NAME': config["DB_NAME"]
+        'NAME': config["DB_NAME"],
+        'HOST': config["DB_HOST"],
+        'PORT': config["DB_PORT"],
+        'USER': config["DB_USER"],
+        'PASSWORD': config["DB_PASSWORD"]
     }
 }
 
